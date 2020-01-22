fe09f6055767d7d855b90020fcb99f42a0a43e1e
Kyle Vaughn <kcvaughn75@gmail.com>
2020-01-22 10:13:58 -0500

    Room Calculator

---
+++ Lab 2 Room Calculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Lab_2_Room_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Room Detail Generator!");
+
+            string userResponse = "y";
+
+            while (userResponse == "y")
+            {
+                Console.WriteLine("Enter Length: ");
+                double length = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Width");
+                double width = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Height");
+                double height = double.Parse(Console.ReadLine());
+
+                double perimeter = (length * 2) + (width * 2);
+
+                double area = (length * width);
+
+                double volume = (length * width * height);
+
+                Console.WriteLine("Perimeter: " + perimeter);
+                Console.WriteLine("Area: " + area);
+                Console.WriteLine("Volume: " + volume);
+
+                Console.WriteLine("Continue? (y/n)");
+                userResponse = Console.ReadLine().ToLower();
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
